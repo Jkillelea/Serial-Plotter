4d775f9d61b0d314c4460a85ff1550bc9f46fc97
Jacob Killelea <jkillelea@protonmail.ch>
2021-12-11 15:14:09 -0600

    Refactor graph drawing code

--- src/widgets/graph_area.rs
+++ src/widgets/graph_area.rs
@@ -49,32 +49,36 @@ impl Widget for GraphArea {
         let alloc = self.widgets.drawing_area.allocation();
         let w     = alloc.width  as f64;
         let h     = alloc.height as f64;
-        // let x0    = alloc.x as f64;
-        // let y0    = alloc.y as f64;
         let x0    = 0.0; // TODO: why is alloc.x not 0?
         let y0    = 0.0; // TODO: why is alloc.y not 0?
+        // let x0    = alloc.x as f64;
+        // let y0    = alloc.y as f64;
         let xmax  = x0 + w;
         let ymax  = y0 + h;
 
-        let context = self.model.draw_handler.get_context().unwrap();
+        if let Ok(context) = self.model.draw_handler.get_context() {
+            context.set_source_rgb(self.model.color.0, self.model.color.1, self.model.color.2);
+            context.move_to(x0, y0 + 0.5 * h);
 
-        context.set_source_rgb(self.model.color.0, self.model.color.1, self.model.color.2);
-        context.move_to(x0, y0 + 0.5 * h);
+            // Draw a sine
+            let phase = 0.0;
+            let freq  = 2.0;
+            let ampl  = h / 2.0;
 
-        // Draw a sine
-        let points = 50;
-        let phase  = 0.0;
-        let freq   = 2.0;
-        let ampl   = h / 2.0;
+            // Sine wave fidelity should scale with screen (diagonal) size
+            let points = (w.powf(2.0) + h.powf(2.0)).sqrt() as i32;
 
-        (0 .. points).map(|i| {
-            let x = x0 + w * (i as f64) / points as f64;
-            let y = y0 + 0.5 * h + ampl * (2.0 * PI * freq * x / xmax + phase).sin();
-            (x, y)
-        })
-        .for_each(|(x, y)| context.line_to(x, y) );
+            (0 .. points).map(|i| {
+                let x = x0 + w * (i as f64) / points as f64;
+                let y = y0 + 0.5 * h + ampl * (2.0 * PI * freq * x / xmax + phase).sin();
+                (x, y)
+            })
+            .for_each(|(x, y)| context.line_to(x, y) );
 
-        context.stroke().unwrap();
+            if let Err(e) = context.stroke() {
+                eprintln!("Drawing error: {}", e);
+            }
+        }
     }
 
     view! {
