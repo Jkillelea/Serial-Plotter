96e335f019d902a4606eddcae8cf875f76cc2e03
Jacob Killelea <jkillelea344@gmail.com>
2022-09-16 08:11:04 -0700

    Improve graph

--- src/widgets/graph_area.rs
+++ src/widgets/graph_area.rs
@@ -50,41 +50,21 @@ impl Widget for GraphArea {
 
         // Set up the drawing area context
         let alloc = self.widgets.drawing_area.allocation();
-        let w = alloc.width() as f64;
-        let h = alloc.height() as f64;
-        let x0 = 0.0; // TODO: why is alloc.x not 0?
-        let y0 = 0.0; // TODO: why is alloc.y not 0?
-        let xmax = x0 + w;
-        let ymax = y0 + h;
-        // let x0    = alloc.x as f64;
-        // let y0    = alloc.y as f64;
+        let w     = alloc.width()  as f64;
+        let h     = alloc.height() as f64;
+        let x0    = alloc.x()      as f64;
+        let y0    = alloc.y()      as f64;
+        let xmax  = x0 + w;
+        let ymax  = y0 + h;
 
         if let Ok(context) = self.model.draw_handler.get_context() {
-            context.set_source_rgba(self.model.color.red(),
+            context.set_source_rgb(self.model.color.red(),
                                     self.model.color.green(),
-                                    self.model.color.blue(),
-                                    self.model.color.alpha());
-            context.move_to(x0, y0 + 0.5 * h);
+                                    self.model.color.blue());
 
-            // // Draw a sine
-            // let point_density = 5.0;
-            // let phase = 0.0;
-            // let freq = 5.0;
-            // let ampl = h / 2.0;
-
-            // // Sine wave fidelity should scale with screen (diagonal) size
-            // let points = ((w.powf(2.0) + h.powf(2.0)).sqrt() / point_density) as i32;
-
-            // (0..points)
-            //     .map(|i| {
-            //         let x = x0 + w * (i as f64) / points as f64;
-            //         let y = y0 + 0.5 * h + ampl * (2.0 * PI * freq * x / xmax + phase).sin();
-            //         (x, y)
-            //     })
-            //     .for_each(|(x, y)| context.line_to(x, y));
-
+            // Plot the points
             for (x, &d) in self.model.data.iter().enumerate() {
-                context.line_to(10.0 * x as f64, d as f64)
+                context.line_to(x as f64, d as f64)
             }
 
             if let Err(e) = context.stroke() {
